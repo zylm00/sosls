3a8d5e5661807cf0b2ade10baf07308d8a596fe1
zylm00 <1123317122@qq.com>
2026-05-12 16:55:08 +0800

    Adjust window size to 270x380

--- flutter/lib/main.dart
+++ flutter/lib/main.dart
@@ -156,7 +156,7 @@ void runMainApp(bool startService) async {
   // Set window option.
   WindowOptions windowOptions = getHiddenTitleBarWindowOptions(
       isMainWindow: true, 
-      size: Size(290, 400),
+      size: Size(270, 380),
       alwaysOnTop: alwaysOnTop);
   windowManager.waitUntilReadyToShow(windowOptions, () async {
     // Restore the location of the main window before window hide or show.
